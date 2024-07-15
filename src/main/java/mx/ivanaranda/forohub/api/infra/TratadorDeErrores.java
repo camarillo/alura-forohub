844dbcdce2276324da7c0f0bad513568893307ab
Ivan Aranda <camarillo.ivan@gmail.com>
2024-07-15 01:30:54 -0600

    feat: Se agrega TratadoDeErrores para las validaciones de las entidades.

--- src/main/java/mx/ivanaranda/forohub/api/infra/TratadorDeErrores.java
+++ src/main/java/mx/ivanaranda/forohub/api/infra/TratadorDeErrores.java
@@ -2,6 +2,10 @@ package mx.ivanaranda.forohub.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,4 +16,21 @@ public class TratadorDeErrores {
     public ResponseEntity tratarError404(){
         return ResponseEntity.notFound().build();
     }
+
+    // Se pueden obtener los datos de la misma excepcion, colocando la MISMA excepcion como parametro
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity manejarError400(MethodArgumentNotValidException e){
+        var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    private record DatosErrorValidacion(
+            String campo
+            , String error
+    ){
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
 }
